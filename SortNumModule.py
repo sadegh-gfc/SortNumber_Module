457271b528c9c5fd5c53e6caa945c30b9f391026
sadegh-gfc <sadeghehsangfc@gmail.com>
2022-07-05 18:48:58 +0430

    Fix some useless code and bugs

--- SortNumModule.py
+++ SortNumModule.py
@@ -1,48 +1,46 @@
 #SortNumber module made by sadegh_gfc
-from itertools import count
 class SortNumber():
     def SortNumber_SmallToBig(numbersList = []):
         numbersList_count = len(numbersList)
         #The "i for" Because if you dont put this if your operation just work for 1 time for exam: numbersList = [5, 7, 3 ,4 ,8, 9 ,1 ,6, 2, 0] if you dont appent this "i for" you renderd this: [5 3 4 7 8 1 6 2 0 9]
         # the "j for" Because Sorting
-        for i in range(0,numbersList_count):  
-            for j in range(0,numbersList_count -1):
-                zarf1 = numbersList[j]
-                zarf2 = numbersList[j + 1]
-                zarf3 = None
-                if zarf1 > zarf2:
-                    zarf3 = zarf1
-                    zarf1 = zarf2
-                    zarf2 = zarf3
+        for i in range(numbersList_count):  
+            for j in range(numbersList_count -1):
+                repositori1 = numbersList[j]
+                repositori2 = numbersList[j + 1]
+                repositori3 = None
+                if repositori1 > repositori2:
+                    repositori3 = repositori1
+                    repositori1 = repositori2
+                    repositori2 = repositori3
 
-                    numbersList[j] = zarf1
-                    numbersList[j +1] = zarf2
-                elif zarf1 < zarf2:
+                    numbersList[j] = repositori1
+                    numbersList[j +1] = repositori2
+                elif repositori1 < repositori2:
                     continue
+                #cahe unexpected Erorr
                 else: 
                     print("SomeThing Wrong in SortNumber class")
-                j = j + 1
         return numbersList
      
     def SortNumber_BigToSmall(numbersList = []):
         numbersList_count = len(numbersList)
-        for i in range(0,numbersList_count):  
-            for j in range(0,numbersList_count -1):
-                zarf1 = numbersList[j]
-                zarf2 = numbersList[j + 1]
-                zarf3 = None
-                if zarf1 < zarf2:
-                    zarf3 = zarf1
-                    zarf1 = zarf2
-                    zarf2 = zarf3
+        for i in range(numbersList_count):  
+            for j in range(numbersList_count -1):
+                repositori1 = numbersList[j]
+                repositori2 = numbersList[j + 1]
+                repositori3 = None
+                if repositori1 < repositori2:
+                    repositori3 = repositori1
+                    repositori1 = repositori2
+                    repositori2 = repositori3
 
-                    numbersList[j] = zarf1
-                    numbersList[j +1] = zarf2
-                elif zarf1 > zarf2:
+                    numbersList[j] = repositori1
+                    numbersList[j +1] = repositori2
+                elif repositori1 > repositori2:
                     continue
                 else: 
                     print("SomeThing Wrong in SortNumber class")
-                j = j + 1
         return numbersList
 
 
