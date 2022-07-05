fda13d4052cbe248411269547bc01a34a3d975c5
sadegh-gfc <sadeghehsangfc@gmail.com>
2022-07-05 18:50:28 +0430

    Make Wider Test situation

--- Demo_SortNumber.py
+++ Demo_SortNumber.py
@@ -1,5 +1,6 @@
-from SortNumber_Module.SortNumModule import SortNumber
+from SortNumModule import SortNumber
 
-numberList = [5, 7, 3 ,4 ,8, 9 ,1 ,6, 2, 0]
+numberList = [5, 7, 3 ,4 ,8, 9 ,1 ,6, 2, 0, -1, -6 , -3]
 
 print (SortNumber.SortNumber_BigToSmall(numbersList=numberList))
+print (SortNumber.SortNumber_SmallToBig(numbersList=numberList))
