a874f4327705e8ac5ec73fb65825874cad68a989
sadegh-gfc <sadeghehsangfc@gmail.com>
2022-07-06 16:23:26 +0430

    improvement Functionaliti of Demo File

--- Demo_SortNumber.py
+++ Demo_SortNumber.py
@@ -1,6 +1,12 @@
 from SortNumModule import SortNumber
-
-numberList = [5, 7, 3 ,4 ,8, 9 ,1 ,6, 2, 0, -1, -6 , -3]
-
-print (SortNumber.SortNumber_BigToSmall(numbersList=numberList))
+# Define our variable we want to be sort (Thats Should be a List!)
+numberList = [5, 7, 3 ,4 ,8, 9 ,1 ,6, 2, 0, -1, -6 , -3,True]
+#print (SortNumber.SortNumber_BigToSmall(numbersList=numberList))
 print (SortNumber.SortNumber_SmallToBig(numbersList=numberList))
+print (SortNumber.SortNumber_BigToSmall(numbersList=numberList))
+
+
+
+
+
+
